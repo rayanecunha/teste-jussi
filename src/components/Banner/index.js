513d03d3deab58a53aabb5f64295f0ab66ee465c
Rayane Gomes <raayanegomes@hotmail.com>
2021-04-23 15:27:25 -0300

    feat: create banner component

--- src/components/Banner/index.js
+++ src/components/Banner/index.js
@@ -1,10 +1,18 @@
 import './styles.scss'
+import produtos from '../../assets/images/produtos-banner.png'
 
 function Banner() {
   return (
     <div className="banner">
-      
-    </div>
+        <div className="text-banner">
+          <h1>Criamos lojas que vendem mais.</h1>
+          <p>A Jüssi é especialista na criação de lojas usando a plataforma VTEX. Precisa criar sua loja ou migrar de plataforma?</p>
+          <a href="/">Veja nossas soluções</a>
+        </div>
+        <div className="product-banner">
+          <img src={produtos} alt="Produtos Banner" />
+        </div>
+      </div>
   );
 }
 
