996149d38e08d921050d7490e29b9514ed94b3ad
Rayane Gomes <raayanegomes@hotmail.com>
2021-04-23 15:27:48 -0300

    feat: create contact component

---
+++ src/components/Contact/index.js
@@ -0,0 +1,13 @@
+import './styles.scss'
+
+function Contact() {
+  return (
+    <div className="contact">
+      <p>Essa loja foi construída usando uma das nossas soluções da plataforma VTEX. tenha a sua.</p>
+      <p className="for-contact">Entre em contato</p>
+      <p className="contact-email">comercial@jussi.com.br</p>
+    </div>
+  );
+}
+
+export default Contact;
